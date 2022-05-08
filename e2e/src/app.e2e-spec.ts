cadec96696b261c4400842e94da5410a72d5a24e
Ralf <bralfi11@gmail.com>
2022-05-08 09:44:42 +0200

    init

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('projekt app is running!');
+    expect(page.getTitleText()).toEqual('fishing-webshop app is running!');
   });
 
   afterEach(async () => {
